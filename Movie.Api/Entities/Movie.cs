970616fa7ebf253ba2e961ba4c52ef69dc41f6f6
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-02-06 23:45:39 +0545

    chore: add Movie model

---
+++ Movie.Api/Entities/Movie.cs
@@ -0,0 +1,12 @@
+namespace Movie.Api.Entities;
+
+public sealed class Movie
+{
+    public required string Id { get; set; }
+    public required string Name { get; set; } = string.Empty;
+    public required DateOnly ReleaseDate { get; set; }
+
+    public string? Description { get; set; }
+    public required decimal Price { get; set; }
+    public required string Genre { get; set; } = string.Empty;
+}
