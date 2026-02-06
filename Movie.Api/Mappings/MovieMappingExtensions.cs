9fd69ab0cf0636b9a5bef06647e2a34da4bef660
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-02-07 00:12:42 +0545

    feat: add mapping into Entity and Response contract manually

---
+++ Movie.Api/Mappings/MovieMappingExtensions.cs
@@ -0,0 +1,42 @@
+using Movie.Api.Contracts.Requests;
+using Movie.Api.Contracts.Responses;
+
+namespace Movie.Api.Mappings;
+
+public static class MovieMappingExtensions
+{
+    public static Entities.Movie ToEntity(this CreateMovieRequest request)
+    {
+        return new Entities.Movie
+        {
+            Id = $"m_{Guid.CreateVersion7()}",
+            Name = request.Name,
+            ReleaseDate = request.ReleaseDate,
+            Description = request.Description,
+            Price = request.Price,
+            Genre = request.Genre
+        };
+    }
+
+    public static MovieResponse ToResponse(this Entities.Movie response)
+    {
+        return new MovieResponse
+        {
+            Id = response.Id,
+            Name = response.Name,
+            ReleaseDate = response.ReleaseDate,
+            Description = response.Description,
+            Price = response.Price,
+            Genre = response.Genre
+        };
+    }
+
+    public static void UpdateFrom(this Entities.Movie entity, UpdateMovieRequest request)
+    {
+        entity.Name = request.Name;
+        entity.ReleaseDate = request.ReleaseDate;
+        entity.Description = request.Description;
+        entity.Price = request.Price;
+        entity.Genre = request.Genre;
+    }
+}
