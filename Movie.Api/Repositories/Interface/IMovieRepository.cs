96f33f55e49737ba0e6d274d64de64355c3766bc
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-02-07 00:26:30 +0545

    feat: interface for movies repository

---
+++ Movie.Api/Repositories/Interface/IMovieRepository.cs
@@ -0,0 +1,10 @@
+namespace Movie.Api.Repositories.Interface;
+
+public interface IMovieRepository
+{
+    Task<IEnumerable<Entities.Movie>> GetAllAsync(CancellationToken ct = default);
+    Task<Entities.Movie?> GetByIdAsync(string id, CancellationToken ct = default);
+    Task AddAsync(Entities.Movie movie, CancellationToken ct = default);
+    Task UpdateAsync(Entities.Movie movie, CancellationToken ct = default);
+    Task DeleteAsync(string id, CancellationToken ct = default);
+}
