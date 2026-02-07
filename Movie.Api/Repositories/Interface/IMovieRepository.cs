8de34b86942cf76313fd56480afc786bd33df644
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2026-02-07 13:13:15 +0545

    chore: add query support

--- Movie.Api/Repositories/Interface/IMovieRepository.cs
+++ Movie.Api/Repositories/Interface/IMovieRepository.cs
@@ -1,8 +1,10 @@
+using Movie.Api.Contracts.Queries;
+
 namespace Movie.Api.Repositories.Interface;
 
 public interface IMovieRepository
 {
-    Task<IEnumerable<Entities.Movie>> GetAllAsync(CancellationToken ct = default);
+    Task<IEnumerable<Entities.Movie>> GetAllAsync(MoviesQueryParameter query ,CancellationToken ct = default);
     Task<Entities.Movie?> GetByIdAsync(string id, CancellationToken ct = default);
     Task AddAsync(Entities.Movie movie, CancellationToken ct = default);
     Task UpdateAsync(Entities.Movie movie, CancellationToken ct = default);
